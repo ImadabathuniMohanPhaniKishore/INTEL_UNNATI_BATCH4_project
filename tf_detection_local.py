ff022e8f2ee1ff544b8963b6c6e906bb1a05bb75
I M P Kishore <impk.chinababu@gmail.com>
2025-04-05 23:36:52 +0530

    Update tf_detection_local.py

--- tf_detection_local.py
+++ tf_detection_local.py
@@ -3,7 +3,7 @@
 # Tensorflow Object Detection Detector
 """A.I People Counter TensorFlow Implementation."""
 """
-Copyright [2020] [MEHUL SOLANKI]
+updated licence
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
